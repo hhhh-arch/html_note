3a427baed06c32857dbc0c4d80f2342d31cb6802
hhhh-arch <83564106+hhhh-arch@users.noreply.github.com>
2025-08-12 13:41:27 +0800

    Update manifest.json to include new MindMap script and CSS
    - Added mindMap.js and mindMap.css to the manifest for improved functionality and styling.
    - Ensured all necessary scripts and styles are properly integrated into the project structure.

---
+++ libs/mind-elixir/node-menu.js
@@ -0,0 +1,257 @@
+window._iconfont_svg_string_4003484 = '<svg><symbol id="icon-a" viewBox="0 0 1024 1024"><path d="M757.76 665.6q0 20.48 1.536 34.304t7.68 22.016 18.944 12.288 34.304 4.096q-3.072 25.6-15.36 44.032-11.264 16.384-33.28 29.696t-62.976 13.312q-11.264 0-20.48-0.512t-17.408-2.56l-6.144-2.048-1.024 0q-4.096-1.024-10.24-4.096-2.048-2.048-4.096-2.048-1.024-1.024-2.048-1.024-14.336-8.192-23.552-17.408t-14.336-17.408q-6.144-10.24-9.216-20.48-63.488 75.776-178.176 75.776-48.128 0-88.064-15.36t-69.12-44.032-45.056-68.096-15.872-88.576 16.896-89.088 47.616-67.584 74.24-42.496 96.768-14.848q48.128 0 88.576 17.408t66.048 49.152q0-8.192 0.512-16.384t0.512-15.36q0-71.68-39.936-104.448t-128-32.768q-43.008 0-84.992 6.656t-84.992 17.92q14.336-28.672 25.088-47.616t24.064-29.184q30.72-24.576 158.72-24.576 79.872 0 135.168 13.824t90.624 43.52 51.2 75.264 15.872 108.032l0 200.704zM487.424 743.424q50.176 0 79.872-33.28t29.696-95.744q0-61.44-28.672-93.696t-76.8-32.256q-52.224 0-82.944 33.28t-30.72 94.72q0 58.368 31.744 92.672t77.824 34.304z"  ></path></symbol><symbol id="icon-close" viewBox="0 0 1024 1024"><path d="M557.312 513.248l265.28-263.904c12.544-12.48 12.608-32.704 0.128-45.248-12.512-12.576-32.704-12.608-45.248-0.128L512.128 467.904l-263.04-263.84c-12.448-12.48-32.704-12.544-45.248-0.064-12.512 12.48-12.544 32.736-0.064 45.28l262.976 263.776L201.6 776.8c-12.544 12.48-12.608 32.704-0.128 45.248a31.937 31.937 0 0 0 22.688 9.44c8.16 0 16.32-3.104 22.56-9.312l265.216-263.808 265.44 266.24c6.24 6.272 14.432 9.408 22.656 9.408a31.94 31.94 0 0 0 22.592-9.344c12.512-12.48 12.544-32.704 0.064-45.248L557.312 513.248z" fill="" ></path></symbol><symbol id="icon-menu" viewBox="0 0 1024 1024"><path d="M109.714 292.571h804.572c21.943 0 36.571-21.942 36.571-43.885 0-14.629-14.628-29.257-36.571-29.257H109.714c-21.943 0-36.571 14.628-36.571 36.571 0 14.629 14.628 36.571 36.571 36.571zM914.286 512H109.714c-21.943 0-36.571 14.629-36.571 36.571 0 14.629 14.628 36.572 36.571 36.572h804.572c21.943 0 36.571-21.943 36.571-43.886 0-14.628-14.628-29.257-36.571-29.257z m0 292.571H109.714c-21.943 0-36.571 14.629-36.571 36.572s14.628 36.571 36.571 36.571h804.572c21.943 0 36.571-21.943 36.571-36.571 0-21.943-14.628-36.572-36.571-36.572z"  ></path></symbol><symbol id="icon-B" viewBox="0 0 1024 1024"><path d="M98.067692 65.457231H481.28c75.854769 0 132.411077 3.150769 169.668923 9.452307 37.336615 6.301538 70.656 19.534769 100.036923 39.620924 29.459692 20.007385 53.956923 46.710154 73.570462 80.029538 19.692308 33.398154 29.459692 70.734769 29.459692 112.167385 0 44.898462-12.130462 86.094769-36.233846 123.588923a224.886154 224.886154 0 0 1-98.461539 84.283077c58.368 17.092923 103.266462 46.08 134.695385 87.04 31.350154 40.96 47.025231 89.088 47.025231 144.462769 0 43.638154-10.082462 86.016-30.404923 127.212308-20.243692 41.196308-47.891692 74.043077-83.02277 98.697846-35.052308 24.654769-78.296615 39.778462-129.732923 45.449846-32.295385 3.465846-110.119385 5.671385-233.472 6.537846H98.067692V65.457231z m193.536 159.507692V446.621538h126.818462c75.460923 0 122.328615-1.024 140.603077-3.229538 33.083077-3.938462 59.155692-15.36 78.139077-34.343385 18.904615-18.904615 28.435692-43.874462 28.435692-74.830769 0-29.696-8.192-53.720615-24.497231-72.310154-16.384-18.510769-40.644923-29.696-72.940307-33.634461-19.140923-2.205538-74.279385-3.308308-165.415385-3.308308h-111.064615z m0 381.243077v256.315077h179.2c69.710769 0 113.979077-1.969231 132.726154-5.907692 28.750769-5.198769 52.145231-17.959385 70.262154-38.281847 18.116923-20.243692 27.096615-47.340308 27.096615-81.368615 0-28.750769-6.931692-53.169231-20.873846-73.255385a118.232615 118.232615 0 0 0-60.494769-43.795692c-26.387692-9.137231-83.574154-13.705846-171.638154-13.705846H291.603692z"  ></path></symbol></svg>', function(u) {
+  var n = (n = document.getElementsByTagName("script"))[n.length - 1], l = n.getAttribute("data-injectcss"), n = n.getAttribute("data-disable-injectsvg");
+  if (!n) {
+    var g, p, C, r, m, t = function(s, c) {
+      c.parentNode.insertBefore(s, c);
+    };
+    if (l && !u.__iconfont__svg__cssinject__) {
+      u.__iconfont__svg__cssinject__ = !0;
+      try {
+        document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>");
+      } catch (s) {
+        console && console.log(s);
+      }
+    }
+    g = function() {
+      var s, c = document.createElement("div");
+      c.innerHTML = u._iconfont_svg_string_4003484, (c = c.getElementsByTagName("svg")[0]) && (c.setAttribute("aria-hidden", "true"), c.style.position = "absolute", c.style.width = 0, c.style.height = 0, c.style.overflow = "hidden", c = c, (s = document.body).firstChild ? t(c, s.firstChild) : s.appendChild(c));
+    }, document.addEventListener ? ~["complete", "loaded", "interactive"].indexOf(document.readyState) ? setTimeout(g, 0) : (p = function() {
+      document.removeEventListener("DOMContentLoaded", p, !1), g();
+    }, document.addEventListener("DOMContentLoaded", p, !1)) : document.attachEvent && (C = g, r = u.document, m = !1, h(), r.onreadystatechange = function() {
+      r.readyState == "complete" && (r.onreadystatechange = null, E());
+    });
+  }
+  function E() {
+    m || (m = !0, C());
+  }
+  function h() {
+    try {
+      r.documentElement.doScroll("left");
+    } catch {
+      return void setTimeout(h, 50);
+    }
+    E();
+  }
+}(window);
+const N = {
+  addChild: "\u63D2\u5165\u5B50\u8282\u70B9",
+  addParent: "\u63D2\u5165\u7236\u8282\u70B9",
+  addSibling: "\u63D2\u5165\u540C\u7EA7\u8282\u70B9",
+  removeNode: "\u5220\u9664\u8282\u70B9",
+  focus: "\u4E13\u6CE8",
+  cancelFocus: "\u53D6\u6D88\u4E13\u6CE8",
+  moveUp: "\u4E0A\u79FB",
+  moveDown: "\u4E0B\u79FB",
+  link: "\u8FDE\u63A5",
+  clickTips: "\u8BF7\u70B9\u51FB\u76EE\u6807\u8282\u70B9",
+  font: "\u6587\u5B57",
+  background: "\u80CC\u666F",
+  tag: "\u6807\u7B7E",
+  icon: "\u56FE\u6807",
+  tagsSeparate: "\u591A\u4E2A\u6807\u7B7E\u534A\u89D2\u9017\u53F7\u5206\u9694",
+  iconsSeparate: "\u591A\u4E2A\u56FE\u6807\u534A\u89D2\u9017\u53F7\u5206\u9694",
+  url: "URL"
+}, i = {
+  cn: N,
+  zh_CN: N,
+  zh_TW: {
+    addChild: "\u63D2\u5165\u5B50\u7BC0\u9EDE",
+    addParent: "\u63D2\u5165\u7236\u7BC0\u9EDE",
+    addSibling: "\u63D2\u5165\u540C\u7D1A\u7BC0\u9EDE",
+    removeNode: "\u522A\u9664\u7BC0\u9EDE",
+    focus: "\u5C08\u6CE8",
+    cancelFocus: "\u53D6\u6D88\u5C08\u6CE8",
+    moveUp: "\u4E0A\u79FB",
+    moveDown: "\u4E0B\u79FB",
+    link: "\u9023\u63A5",
+    clickTips: "\u8ACB\u9EDE\u64CA\u76EE\u6A19\u7BC0\u9EDE",
+    font: "\u6587\u5B57",
+    background: "\u80CC\u666F",
+    tag: "\u6A19\u7C3D",
+    icon: "\u5716\u6A19",
+    tagsSeparate: "\u591A\u500B\u6A19\u7C3D\u534A\u89D2\u9017\u865F\u5206\u9694",
+    iconsSeparate: "\u591A\u500B\u5716\u6A19\u534A\u89D2\u9017\u865F\u5206\u9694",
+    url: "URL"
+  },
+  en: {
+    addChild: "Add child",
+    addParent: "Add parent",
+    addSibling: "Add sibling",
+    removeNode: "Remove node",
+    focus: "Focus Mode",
+    cancelFocus: "Cancel Focus Mode",
+    moveUp: "Move up",
+    moveDown: "Move down",
+    link: "Link",
+    clickTips: "Please click the target node",
+    font: "Font",
+    background: "Background",
+    tag: "Tag",
+    icon: "Icon",
+    tagsSeparate: "Separate tags by comma",
+    iconsSeparate: "Separate icons by comma",
+    url: "URL"
+  },
+  ru: {
+    addChild: "\u0414\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u0434\u043E\u0447\u0435\u0440\u043D\u0438\u0439 \u044D\u043B\u0435\u043C\u0435\u043D\u0442",
+    addParent: "\u0414\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u0440\u043E\u0434\u0438\u0442\u0435\u043B\u044C\u0441\u043A\u0438\u0439 \u044D\u043B\u0435\u043C\u0435\u043D\u0442",
+    addSibling: "\u0414\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u043D\u0430 \u044D\u0442\u043E\u043C \u0443\u0440\u043E\u0432\u043D\u0435",
+    removeNode: "\u0423\u0434\u0430\u043B\u0438\u0442\u044C \u0443\u0437\u0435\u043B",
+    focus: "\u0420\u0435\u0436\u0438\u043C \u0444\u043E\u043A\u0443\u0441\u0438\u0440\u043E\u0432\u043A\u0438",
+    cancelFocus: "\u041E\u0442\u043C\u0435\u043D\u0438\u0442\u044C \u0440\u0435\u0436\u0438\u043C \u0444\u043E\u043A\u0443\u0441\u0438\u0440\u043E\u0432\u043A\u0438",
+    moveUp: "\u041F\u043E\u0434\u043D\u044F\u0442\u044C \u0432\u044B\u0448\u0435",
+    moveDown: "\u041E\u043F\u0443\u0441\u0442\u0438\u0442\u044C \u043D\u0438\u0436\u0435",
+    link: "\u0421\u0441\u044B\u043B\u043A\u0430",
+    clickTips: "\u041F\u043E\u0436\u0430\u043B\u0443\u0439\u0441\u0442\u0430, \u043D\u0430\u0436\u043C\u0438\u0442\u0435 \u043D\u0430 \u0446\u0435\u043B\u0435\u0432\u043E\u0439 \u0443\u0437\u0435\u043B",
+    font: "\u0426\u0432\u0435\u0442 \u0448\u0440\u0438\u0444\u0442\u0430",
+    background: "\u0426\u0432\u0435\u0442 \u0444\u043E\u043D\u0430",
+    tag: "\u0422\u0435\u0433",
+    icon: "\u0418\u043A\u043E\u043D\u043A\u0430",
+    tagsSeparate: "\u0420\u0430\u0437\u0434\u0435\u043B\u044F\u0439\u0442\u0435 \u0442\u0435\u0433\u0438 \u0437\u0430\u043F\u044F\u0442\u043E\u0439",
+    iconsSeparate: "\u0420\u0430\u0437\u0434\u0435\u043B\u044F\u0439\u0442\u0435 \u0438\u043A\u043E\u043D\u043A\u0438 \u0437\u0430\u043F\u044F\u0442\u043E\u0439"
+  },
+  ja: {
+    addChild: "\u5B50\u30CE\u30FC\u30C9\u3092\u8FFD\u52A0\u3059\u308B",
+    addParent: "\u89AA\u30CE\u30FC\u30C9\u3092\u8FFD\u52A0\u3057\u307E\u3059",
+    addSibling: "\u5144\u5F1F\u30CE\u30FC\u30C9\u3092\u8FFD\u52A0\u3059\u308B",
+    removeNode: "\u30CE\u30FC\u30C9\u3092\u524A\u9664",
+    focus: "\u96C6\u4E2D",
+    cancelFocus: "\u96C6\u4E2D\u89E3\u9664",
+    moveUp: "\u4E0A\u3078\u79FB\u52D5",
+    moveDown: "\u4E0B\u3078\u79FB\u52D5",
+    link: "\u30B3\u30CD\u30AF\u30C8",
+    clickTips: "\u30BF\u30FC\u30B2\u30C3\u30C8\u30CE\u30FC\u30C9\u3092\u30AF\u30EA\u30C3\u30AF\u3057\u3066\u304F\u3060\u3055\u3044",
+    font: "\u30D5\u30A9\u30F3\u30C8",
+    background: "\u30D0\u30C3\u30AF\u30B0\u30E9\u30A6\u30F3\u30C9",
+    tag: "\u30BF\u30B0",
+    icon: "\u30A2\u30A4\u30B3\u30F3",
+    tagsSeparate: "\u8907\u6570\u30BF\u30B0\u306F\u30AB\u30F3\u30DE\u533A\u5207\u308A",
+    iconsSeparate: "\u8907\u6570\u30A2\u30A4\u30B3\u30F3\u306F\u30AB\u30F3\u30DE\u533A\u5207\u308A",
+    url: "URL"
+  },
+  pt: {
+    addChild: "Adicionar item filho",
+    addParent: "Adicionar item pai",
+    addSibling: "Adicionar item irmao",
+    removeNode: "Remover item",
+    focus: "Modo Foco",
+    cancelFocus: "Cancelar Modo Foco",
+    moveUp: "Mover para cima",
+    moveDown: "Mover para baixo",
+    link: "Link",
+    clickTips: "Favor clicar no item alvo",
+    font: "Fonte",
+    background: "Cor de fundo",
+    tag: "Tag",
+    icon: "Icone",
+    tagsSeparate: "Separe tags por virgula",
+    iconsSeparate: "Separe icones por virgula",
+    url: "URL"
+  }
+}, v = (u, l) => {
+  const n = document.createElement("div");
+  return n.id = u, n.innerHTML = l, n;
+}, S = [
+  "#2c3e50",
+  "#34495e",
+  "#7f8c8d",
+  "#94a5a6",
+  "#bdc3c7",
+  "#ecf0f1",
+  "#8e44ad",
+  "#9b59b6",
+  "#2980b9",
+  "#3298db",
+  "#c0392c",
+  "#e74c3c",
+  "#d35400",
+  "#f39c11",
+  "#f1c40e",
+  "#17a085",
+  "#27ae61",
+  "#2ecc71"
+];
+function k(u) {
+  console.log("install node menu");
+  function l(e, a) {
+    const o = u.container.querySelectorAll(e);
+    [].forEach.call(o, function(d) {
+      d.classList.remove(a);
+    });
+  }
+  const n = i[u.locale] ? u.locale : "en", g = v(
+    "nm-style",
+    `
+  <div class="nm-fontsize-container">
+    ${["15", "24", "32"].map((e) => `<div class="size"  data-size="${e}">
+    <svg class="icon" style="width: ${e}px;height: ${e}px" aria-hidden="true">
+      <use xlink:href="#icon-a"></use>
+    </svg></div>`).join("")}<div class="bold"><svg class="icon" aria-hidden="true">
+<use xlink:href="#icon-B"></use>
+</svg></div>
+  </div>
+  <div class="nm-fontcolor-container">
+    ${S.map((e) => `<div class="split6"><div class="palette" data-color="${e}" style="background-color: ${e};"></div></div>`).join("")}
+  </div>
+  <div class="bof">
+  <span class="font">${i[n].font}</span>
+  <span class="background">${i[n].background}</span>
+  </div>`
+  ), p = v("nm-tag", `${i[n].tag}<input class="nm-tag" tabindex="-1" placeholder="${i[n].tagsSeparate}" />`), C = v("nm-icon", `${i[n].icon}<input class="nm-icon" tabindex="-1" placeholder="${i[n].iconsSeparate}" />`), r = v("nm-url", `${i[n].url}<input class="nm-url" tabindex="-1" />`), m = v("nm-memo", `${i[n].memo || "Memo"}<textarea class="nm-memo" rows="5" tabindex="-1" />`), t = document.createElement("div");
+  t.className = "node-menu", t.innerHTML = `
+  <div class="button-container"><svg class="icon" aria-hidden="true">
+  <use xlink:href="#icon-close"></use>
+  </svg></div>
+  `, t.appendChild(g), t.appendChild(p), t.appendChild(C), t.appendChild(r), t.appendChild(m), t.hidden = !0, u.container.append(t);
+  const E = t.querySelectorAll(".size"), h = t.querySelector(".bold"), s = t.querySelector(".button-container"), c = t.querySelector(".font"), A = u.container.querySelector(".nm-tag"), B = u.container.querySelector(".nm-icon"), y = u.container.querySelector(".nm-url"), D = u.container.querySelector(".nm-memo");
+  let f;
+  u.findEle, t.onclick = (e) => {
+    if (!u.currentNode)
+      return;
+    const a = u.currentNode.nodeObj, o = e.target;
+    if (o.className === "palette") {
+      l(".palette", "nmenu-selected"), o.className = "palette nmenu-selected";
+      const d = o.dataset.color, F = { style: {} };
+      f === "font" ? F.style.color = d : f === "background" && (F.style.background = d), console.log(F), u.reshapeNode(u.currentNode, F);
+    } else
+      o.className === "background" ? (l(".palette", "nmenu-selected"), f = "background", o.className = "background selected", o.previousElementSibling.className = "font", a.style && a.style.background && (t.querySelector('.palette[data-color="' + a.style.background + '"]').className = "palette nmenu-selected")) : o.className === "font" && (l(".palette", "nmenu-selected"), f = "font", o.className = "font selected", o.nextElementSibling.className = "background", a.style && a.style.color && (t.querySelector('.palette[data-color="' + a.style.color + '"]').className = "palette nmenu-selected"));
+  }, Array.from(E).map((e) => {
+    e.onclick = (a) => {
+      l(".size", "size-selected");
+      const o = a.currentTarget;
+      o.className = "size size-selected", u.reshapeNode(u.currentNode, { style: { fontSize: o.dataset.size } });
+    };
+  }), h.onclick = (e) => {
+    var o, d;
+    let a = "";
+    ((d = (o = u.currentNode.nodeObj) == null ? void 0 : o.style) == null ? void 0 : d.fontWeight) === "bold" ? e.currentTarget.className = "bold" : (a = "bold", e.currentTarget.className = "bold size-selected"), u.reshapeNode(u.currentNode, { style: { fontWeight: a } });
+  }, A.onchange = (e) => {
+    if (!!u.currentNode && typeof e.target.value == "string") {
+      const a = e.target.value.split(",");
+      u.reshapeNode(u.currentNode, { tags: a.filter((o) => o) });
+    }
+  }, B.onchange = (e) => {
+    if (!!u.currentNode && typeof e.target.value == "string") {
+      const a = e.target.value.split(",");
+      u.reshapeNode(u.currentNode, { icons: a.filter((o) => o) });
+    }
+  }, y.onchange = (e) => {
+    !u.currentNode || u.reshapeNode(u.currentNode, { hyperLink: e.target.value });
+  }, D.onchange = (e) => {
+    !u.currentNode || (u.currentNode.nodeObj.memo = e.target.value, u.bus.fire("operation", {
+      name: "updateMemo",
+      obj: u.currentNode.nodeObj
+    }));
+  };
+  let b = "open";
+  s.onclick = (e) => {
+    t.classList.toggle("close"), b === "open" ? (b = "close", s.innerHTML = '<svg class="icon" aria-hidden="true"><use xlink:href="#icon-menu"></use></svg>') : (b = "open", s.innerHTML = '<svg class="icon" aria-hidden="true"><use xlink:href="#icon-close"></use></svg>');
+  }, u.bus.addListener("unselectNode", function() {
+    t.hidden = !0;
+  }), u.bus.addListener("selectNode", function(e, a) {
+    !a || (t.hidden = !1, l(".palette", "nmenu-selected"), l(".size", "size-selected"), l(".bold", "size-selected"), f = "font", c.className = "font selected", c.nextElementSibling.className = "background", e.style && (e.style.fontSize && (t.querySelector('.size[data-size="' + e.style.fontSize + '"]').className = "size size-selected"), e.style.fontWeight && (t.querySelector(".bold").className = "bold size-selected"), e.style.color && (t.querySelector('.palette[data-color="' + e.style.color + '"]').className = "palette nmenu-selected")), e.tags ? A.value = e.tags.join(",") : A.value = "", e.icons ? B.value = e.icons.join(",") : B.value = "", y.value = e.hyperLink || "", D.value = e.memo || "");
+  });
+}
+export {
+  k as default
+};
