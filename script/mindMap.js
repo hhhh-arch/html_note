f78efffbc8d4ee2173add4d5c0133fec95a54295
hhhh-arch <83564106+hhhh-arch@users.noreply.github.com>
2025-08-12 16:29:37 +0800

    Update manifest.json and refactor MindElixir integration
    - Added background service worker configuration in manifest.json for improved performance.
    - Replaced deprecated MindElixir.js and MindElixirUmb.js with updated module versions in the content scripts.
    - Enhanced mindMap.js to initialize the MindElixir instance with new options and improved user interaction features.
    - Removed obsolete node-menu and mind-elixir files to streamline the project structure.

--- script/mindMap.js
+++ script/mindMap.js
@@ -5,6 +5,59 @@ function showMindMapPanel() {
     const panel = document.createElement('div');
     panel.id = 'mindmap-panel';
     
-    panel.innerHTML = `<div id="mindmap-container"><script src="libs/mind-elixir/MindElixirUmb.js"></script></div>`;
+    panel.innerHTML = `<div id="mindmap-container">
+    <div id="map"></div>
+<style>
+  #map {
+    height: 500px;
+    width: 100%;
+  }
+</style>
+    </div>`;
     document.body.appendChild(panel);
+    console.log('MindElixir:', window.MindElixir);
+    const MindElixir = window.MindElixir.default;
+    let options = {
+        el: '#map', // or HTMLDivElement
+        direction: MindElixir.LEFT,
+        draggable: true, // default true
+        contextMenu: true, // default true
+        toolBar: true, // default true
+        nodeMenu: true, // default true
+        keypress: true, // default true
+        locale: 'en', // [zh_CN,zh_TW,en,ja,pt,ru] waiting for PRs
+        overflowHidden: false, // default false
+        mainLinkStyle: 2, // [1,2] default 1
+        mouseSelectionButton: 0, // 0 for left button, 2 for right button, default 0
+        contextMenuOption: {
+          focus: true,
+          link: true,
+          extend: [
+            {
+              name: 'Node edit',
+              onclick: () => {
+                alert('extend menu')
+              },
+            },
+          ],
+        },
+        before: {
+          insertSibling(type, obj) {
+            return true
+          },
+        },
+      }
+      
+      let mind = new MindElixir(options)
+      
+      mind.install(window.NodeMenu) // install your plugin
+      
+      // create new map data
+      const data = MindElixir.new('new topic')
+      // or `example`
+      // or the data return from `.getData()`
+      mind.init(data)
+      
+      // get a node
+      MindElixir.E('node-id')
   }
